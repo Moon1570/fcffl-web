d624634b451cde952833b6aa38acef8065cde6e7
Abu Sayeed Md Afridi <39995805+afridi15@users.noreply.github.com>
2023-06-15 19:04:30 +0600

    can edit admin-user information

--- fcfl-admin-dir/passrec.php
+++ fcfl-admin-dir/passrec.php
@@ -1,1 +1,17 @@
-<?php echo "hello". $email;?>
+<?php
+include '../include/db_connection.php';
+session_start();
+
+if(!isset($_SESSION["aid"])) {
+    header("Location:index.php");
+  }
+$conn = OpenCon();
+$sql = "UPDATE admin SET username='".$_POST['username']."', email='".$_POST['email']."', firstname='".$_POST['firstname']."', lastname='".$_POST['lastname']."', address='".$_POST['address']."', city='".$_POST['city']."', country='".$_POST['country']."', postal_code='".$_POST['postal_code']."', about_me='".$_POST['about_me']."' WHERE aid = '".$_SESSION["aid"]."'";
+$result = mysqli_query($conn, $sql);
+if($result) {
+    header("Location:user.php");
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+CloseCon($conn); 
+?>
